0c017989c8d427f84f375cd85bbb5c3037109f6e
Scott Kim <scott102938@gmail.com>
2020-07-29 17:22:04 +0900

    Updates

---
+++ precache-manifest.dc9557aea9b0b968e4643a5ccdd0fd54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1222f3d1328b9b4565cf9498a987e6aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68e2468e183db11a56c3",
+    "url": "/static/css/main.d72eadaa.chunk.css"
+  },
+  {
+    "revision": "f7513e1fd22ef664b998",
+    "url": "/static/js/2.076690f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.076690f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e2468e183db11a56c3",
+    "url": "/static/js/main.3d05dcc1.chunk.js"
+  },
+  {
+    "revision": "26ca9527d0451f9eead8",
+    "url": "/static/js/runtime-main.0e255956.js"
+  },
+  {
+    "revision": "9e881fd7f2705f3caa46e3e247ed73c7",
+    "url": "/static/media/me.9e881fd7.jpg"
+  }
+]);
