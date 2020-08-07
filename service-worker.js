6b0902aca65a9058a2322a5a64978052c0624a2b
Scott Kim <scott102938@gmail.com>
2020-08-08 02:03:22 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ee9748d3514f076cd40e33e470e4d5c.js"
+  "/precache-manifest.644f8ea321b9b966dc71a1dd713f1919.js"
 );
 
 self.addEventListener('message', (event) => {
