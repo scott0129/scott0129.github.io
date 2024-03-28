4dcf1f41925e7e4f804386d6a83250123899b15f
Scott Kim <scott102938@gmail.com>
2024-03-28 00:38:20 -0700

    Init vite

---
+++ dev-README.md
@@ -0,0 +1,10 @@
+## Tabs:
+https://vitejs.dev/guide/
+https://console.firebase.google.com/u/0/project/home-2a353/overview
+
+## Doing:
+Just trying to get /something/ deployed to Firebase, and then copying over from old directory.
+
+## Getting Started:
+Start Local Server: `yarn run dev`
+Editor: VSCode
---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsk_TLn4wSJmSTpPe-ZJA__MYMqyG4JC4",
+  authDomain: "home-2a353.firebaseapp.com",
+  projectId: "home-2a353",
+  storageBucket: "home-2a353.appspot.com",
+  messagingSenderId: "753065607602",
+  appId: "1:753065607602:web:7b2e071994bd804d1464ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
