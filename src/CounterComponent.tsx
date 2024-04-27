23b1c365a44303bd108ef5c2bd9c285ae0fc8d75
Scott Kim <scott102938@gmail.com>
2024-04-27 15:01:21 -0700

    Enable localhost emulations

--- src/CounterComponent.tsx
+++ src/CounterComponent.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   doc,
   getDoc,
-  setDoc,
   updateDoc,
   onSnapshot,
   increment,
--- src/LoginComponent.tsx
+++ src/LoginComponent.tsx
@@ -1,10 +1,14 @@
-import { getFunctions, httpsCallable } from 'firebase/functions';
+import { connectFunctionsEmulator, getFunctions, httpsCallable } from 'firebase/functions';
 import app from './firebaseConfig'
 import { useState } from 'react';
 import { getAuth, signInWithCustomToken } from 'firebase/auth';
 import { FirebaseError } from 'firebase/app';
 
 const functions = getFunctions(app);
+if (window.location.hostname === "localhost") {
+  console.log("Connecting to localhost firebase emulator")
+  connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+}
 const auth = getAuth(app);
 
 const LoginComponent = () => {
@@ -17,13 +21,14 @@ const LoginComponent = () => {
     const authenticateUser = httpsCallable(functions, 'authenticateuser')
 
     try {
+      setErrorMessage('');
       const result: any = await authenticateUser({groupChatName, firstName});
       const token = result.data.token;
       await signInWithCustomToken(auth, token);
       console.log("Sign-in successful!");
     } catch (e) {
       if (e instanceof FirebaseError && e.code == 'functions/failed-precondition') {
-        setErrorMessage('Couldn\'t log you in. Are you sure you typed in the group chat correctly? (no emojis)');
+        setErrorMessage('Couldn\'t log you in. Are you sure you typed in the group chat correctly?');
       } else {
         setErrorMessage('Unknown error. Contact Scotto');
         console.error(e);
@@ -36,16 +41,16 @@ const LoginComponent = () => {
 
   return (
     <div>
-      <label>Group Chat Name</label>
-      <input type="text" id="groupChatName" 
-              value={groupChatName}
-              onChange={(e) => setGroupChatName(e.target.value)}
-      />
-      <br/>
-      <label>First Name</label>
+      <label>First Name </label>
       <input type="text" id="firstName" 
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
+      />
+      <br/>
+      <label>Group Chat Name </label>
+      <input type="text" id="groupChatName" 
+              value={groupChatName}
+              onChange={(e) => setGroupChatName(e.target.value)}
       />
       <br/>
       <button onClick={(authenticate)}>Press Me</button>
